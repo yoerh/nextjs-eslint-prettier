d8332db0586316474115cbfbbede9ed32aa415c3
yoerh <yoerh@ya.ru>
2022-11-10 08:18:53 +0300

    start

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default function Home() {
+  const Wrapper = styled.div`
+    background-color: red;
+  `;
+
+  return (
+    <>
+      <Wrapper>Hey</Wrapper>
+    </>
+  );
+}
